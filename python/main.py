aef408336501d6676a818c31dfd46e1861e62bea
Victor Bustamante <victorbustamante12@gmail.com>
2017-06-29 13:56:32 -0300

    Finished python port

--- python/main.py
+++ python/main.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5 import uic, QtWidgets, QtGui
-from PyQt5.QtCore import Qt, QDirIterator, QDir
+from PyQt5.QtCore import Qt, QDirIterator, QDir, QFileInfo, QUrl
+from PyQt5.QtWidgets import QMessageBox, QInputDialog
 
 
 class Ui(QtWidgets.QMainWindow):
@@ -12,7 +13,6 @@ class Ui(QtWidgets.QMainWindow):
 
         self.stackedWidget.setCurrentIndex(0)
         self.Log.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
-        self.Log.setContextMenuPolicy(Qt.CustomContextMenu)
 
         self.Path.setText(self.currentDir.absolutePath())
         self.on_execute_clicked()
@@ -43,10 +43,40 @@ class Ui(QtWidgets.QMainWindow):
         menu.exec(globalpos)
 
     def erase_item(self):
-        print("Deeting")
+        items = self.Log.selectedItems()
+        for item in items:
+            confirmation = QMessageBox.question(self, "RASP", "Are you sure you want to delete " + item.text(), QMessageBox.Yes | QMessageBox.No)
+
+            if confirmation == QMessageBox.Yes:
+                dirtodelete = QDir(item.text())
+                dirtodelete.removeRecursively()
+                self.on_execute_clicked()
 
     def create_item(self):
-        print("Creating")
+        # QtWidgets.QMessageBox.information(self, "RASP", "Directory successfully created.")
+        dirname, ok = QInputDialog.getText(self, "RASP", "Directory Name:")
+        if ok and dirname:
+            if self.currentDir.mkdir(dirname):
+                QMessageBox.information(self, "RASP", "Directory successfully created.")
+                self.on_execute_clicked()
+            else:
+                QMessageBox.information(self, "RASP", "Directory could not be created.")
+
+    def on_item_double_clicked(self, item):
+        iteminfo = QFileInfo(item.text())
+
+        if iteminfo.isDir():
+            self.currentDir.cd(item.text())
+            self.on_execute_clicked()
+            self.Path.setText(self.currentDir.absolutePath())
+        else:
+            QtGui.QDesktopServices.openUrl(QUrl.fromLocalFile(item.text()))
+
+    def on_workspace_item_double_clicked(self, item):
+        if item.text() == "Local":
+            self.stackedWidget.setCurrentIndex(0)
+        elif item.text() == "Server 1":
+            self.stackedWidget.setCurrentIndex(1)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
